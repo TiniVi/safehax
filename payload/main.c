da71af5c653c401bacd0684b72f7e6033fdcbd4e
Andres Puel <TiniVi@rocketmail.com>
2017-05-26 18:46:27 -0700

    r g b -> b g r

--- payload/main.c
+++ payload/main.c
@@ -28,7 +28,7 @@ unsigned int pxi_recv(void){
 }
 
 void _start(void){
-	DEBUG(0xFF0000); //red
+	DEBUG(0x0000FF); //red
 	
 	for (volatile int i = 0; i < 2; i++){ //The global SAFE_MODE flag is cleared now, let's launch SAFE_MODE!
 		/* Init */
@@ -40,7 +40,7 @@ void _start(void){
 		ARM9SYNC[0] = 1;
 		while (ARM9SYNC[0] != 2);
 		
-		DEBUG(0xFF7F00); //orange
+		DEBUG(0x007FFF); //orange
 		
 		if (ARM9SYNC[1] == 3){
 			ARM9SYNC[0] = 1;
@@ -52,7 +52,7 @@ void _start(void){
 		PXI_SYNC11[1] = 1;
 		while (PXI_SYNC11[0] != 1);
 		
-		DEBUG(0xFFFF00); //yellow
+		DEBUG(0x00FFFF); //yellow
 		
 		while (!PXI_EMPTY) pxi_recv();
 		PXI_SYNC11[1] = 2;
@@ -65,7 +65,7 @@ void _start(void){
 		PXI_SYNC11[3] |= 0x40;
 		pxi_send(0x10000); //pxi shutdown
 		
-		DEBUG(0x00FFFF); //cyan
+		DEBUG(0xFFFF00); //cyan
 		
 		do pxi_send(0x44836); //SAFE_MODE Process9 @ 0x08086788 & SAFE_MODE Kernel11 @ 0xFFF620C0
 		while (PXI_EMPTY || (pxi_recv() != 0x964536));
@@ -74,10 +74,10 @@ void _start(void){
 		pxi_send(FW_TID_HIGH);
 		pxi_send(FW_TID_LOW);
 		
-		DEBUG(0x0000FF); //blue
+		DEBUG(0xFF0000); //blue
 	}
 	
-	DEBUG(0x7F00FF); //purple
+	DEBUG(0xFF007F); //purple
 	
 	/* FIRMLaunchHax */
 	ARM11Entry = 0;
